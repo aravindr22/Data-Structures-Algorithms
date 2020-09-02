cc8128d80599b74f96c2e755a5802bd5cd7da1de
Aravind <aravind08222@gmail.com>
2020-09-02 20:00:13 +0530

    Added display and insert at beg to the linked list

--- C++/Data Structures/linkedlist/Single_Linked_List.cpp
+++ C++/Data Structures/linkedlist/Single_Linked_List.cpp
@@ -6,8 +6,36 @@ struct node{
 	int data;
 	struct node *next;
 };
+
 struct node *head;
 
+void insertAtBeg(int x){
+	struct node *temp;
+	temp = new struct node;
+	temp->data=x;
+	temp->next=NULL;
+	if(head == NULL){
+		head=temp;
+	} else {
+		temp->next=head;
+		head=temp;
+	}
+}
+
+void display(){
+	struct node *temp;
+	temp = head;
+	while(temp!=NULL){
+		cout<<temp->data<<endl;
+		temp=temp->next;
+	}
+}
+
 int main(){
+	int x;
 	
+	for(int i=0;i<=10;i++){
+		insertAtBeg(i);
+	}
+	display();
 }
