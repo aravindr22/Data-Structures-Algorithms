79cd9587819d54003afc1ca9fe603b2a2d50d589
Aravind <aravind08222@gmail.com>
2020-09-03 22:30:12 +0530

    added insert at pos

--- C++/Data Structures/linkedlist/Single_Linked_List.cpp
+++ C++/Data Structures/linkedlist/Single_Linked_List.cpp
@@ -36,6 +36,26 @@ void insertAtEnd(int x){
 	}
 }
 
+void insertAtPos(){
+	int x,pos;
+	cin>>x>>pos;
+	struct node *temp;
+	struct node *curr;
+	struct node *temp2;
+	temp = new struct node;
+	temp->data=x;
+	temp->next=NULL;
+	if(head==NULL){
+		head=temp;
+	} else {
+		int i;
+		for(curr=head,i=0;i<pos-1;curr=curr->next,i++);
+		temp2=curr->next;
+		curr->next=temp;
+		temp->next=temp2;
+	}
+}
+
 void display(){
 	struct node *temp;
 	temp = head;
@@ -48,9 +68,10 @@ void display(){
 int main(){
 	int x;
 	
-	for(int i=0;i<=10;i++){
-		insertAtBeg(i);
+	for(int i=0;i<=5;i++){
 		insertAtEnd(i);
 	}
+	display();
+	insertAtPos();
 	display();
 }
