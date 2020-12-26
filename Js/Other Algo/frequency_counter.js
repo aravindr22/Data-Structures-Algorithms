a38b9a19018c28150c590dea57f630ac555c68c2
Aravind <aravind08222@gmail.com>
2020-12-26 14:02:48 +0530

    added comments to frequency counter

--- Js/Other Algo/frequency_counter.js
+++ Js/Other Algo/frequency_counter.js
@@ -1,28 +1,49 @@
+//Frequency counter is also known as Hash map
+//Hash map (or) Frequency counter is implemented using Dictionaries
+//Anagram check is application of frequency counter
+//Anagram Check is where we compare two words where it contains same no.of elements
+//(i.e) school,chsolo is anagram where length and letters are same
+//school,scloh is not an anagram because length and letters are not same
+
 function anagramCheck(arr1,arr2){
+
+  //Checks if length of both strings are same. If not return false
   if(arr1.length !== arr2.length){
     return false;
   }
+
+  //Declaring dictionaries for 2 strings
   arr1_obj = {}
   arr2_obj = {}
 
+  //Mapping each letter to dictionary
+  //for example school is mapped as {'s': 1, 'c': 1, 'h': 1,'o': 2,'l': 1}
   for(let val of arr1){
+
+    //THis line checks if the element is already present or not
+    //If already present increase by 1
+    //If not present creates a key initailize its value to 1
     arr1_obj[val] = (arr1_obj[val] || 0) + 1;
   }
   for(let val of arr2){
     arr2_obj[val] = (arr2_obj[val] || 0) + 1;
   }
-  console.log(arr1_obj,arr2_obj)
+
+  //To check the mapped strings uncomment the below line
+  //console.log(arr1_obj,arr2_obj)
   for(let key in arr1_obj){
-    if(! (key**2 in arr2_obj)){
+    
+    //Checks if element in 1st string present in 2nd string
+    if(!(key in arr2_obj)){
       return false;
     }
-    if(arr2_obj[key**2] !== arr1_obj[key]){
+
+    //Checks if the no.of element in 1st string and no.of element in 2nd string are same
+    if(arr1_obj[key] !== arr2_obj[key]){
       return false;
     }
   }
   return true;
 }
 
-console.log(check([1,2,3,3],[1,9,4,4]))
-//console.log(check([1,2],[1,4]))
-//console.log(check([1,2,1],[1,4,4]))
+console.log(anagramCheck("school","shoolc"))
--- Python/Other algo/frequency_counter.py
+++ Python/Other algo/frequency_counter.py
@@ -1,9 +1,23 @@
+#Frequency counter is also known as Hash map
+#Hash map (or) Frequency counter is implemented using Dictionaries
+#Anagram check is application of frequency counter
+#Anagram Check is where we compare two words where it contains same no.of elements
+#(i.e) school,chsolo is anagram where length and letters are same
+#school,scloh is not an anagram because length and letters are not same
+
+
 def frequencyCheck(arr1,arr2):
+    
+    #Checks if length of both strings are same. If not return false
     if len(arr1) != len(arr2):
         return False
+
+    #Declaring dictionaries for 2 strings
     frequency_obj1 = {}
     frequency_obj2 = {}
 
+    #Mapping each letter to dictionary
+    #for example school is mapped as {'s': 1, 'c': 1, 'h': 1,'o': 2,'l': 1}
     for val in arr1:
         if val in frequency_obj1:
             frequency_obj1[val] = frequency_obj1[val] + 1
@@ -15,13 +29,18 @@ def frequencyCheck(arr1,arr2):
             frequency_obj2[val] = frequency_obj2[val] + 1
         else:
             frequency_obj2[val] = 1
-
-    print(frequency_obj1,frequency_obj2)
+    
+    #To check the mapped strings uncomment the below line
+    #print(frequency_obj1,frequency_obj2)
     for key in frequency_obj1:
-        if(not(key**2 in frequency_obj2)):
+        
+        #Checks if element in 1st string present in 2nd string
+        if(not(key in frequency_obj2)):
             return False
-        if(frequency_obj1[key] != frequency_obj2[key**2]):
+
+        #Checks if the no.of element in 1st string and no.of element in 2nd string are same
+        if(frequency_obj1[key] != frequency_obj2[key]):
             return False
     return True
 
-print(frequencyCheck([1,2,3],[4,4,1]))
+print(frequencyCheck("school","chools"))
