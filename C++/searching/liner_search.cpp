d049976941b2b0eb3aa2f459a5a49871cb227e5d
Aravind <ar22testmail@gmail.com>
2020-12-29 14:50:32 +0530

    update in linear search c++

--- C++/searching/liner_search.cpp
+++ C++/searching/liner_search.cpp
@@ -5,12 +5,13 @@
 #include<iostream>
 using namespace std;
 
-
 void search(int arr[],int n,int k)
 {
+
+    //loop is runned to find the element linearily
     for(int i=0;i<n;i++)
     {
-        if(arr[i]==k)	//loop is runned to find the element linearily
+        if(arr[i]==k)	
         {
             cout<<"the index is found at "<<i;   // the index is printed
         }
@@ -22,14 +23,14 @@ int main()
 {
     int arr[100],n;
     cout<<"enter the number of elements:";
-    cin>>n;//number of elements are received
+    cin>>n;                                     //number of elements are received
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i];	//array elements are received
+        cin>>arr[i];	                        //array elements are received
     }
     int k;
     cout<<"enter the key value";
-    cin>>k;			//key value is received
-    search(arr,n,k);	//function is called	
+    cin>>k;                         			//key value is received
+    search(arr,n,k);	                        //function is called	
     return 0;
 }
